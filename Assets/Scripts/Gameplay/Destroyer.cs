b59a7556e645d2c8940af8fe3355eeca39143558
alfianAH <alfian.hamdani04@gmail.com>
2022-04-22 16:32:33 +0800

    Scripting destroyer

---
+++ Assets/Scripts/Gameplay/Destroyer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Destroyer : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            
+        }
+    }
+}
