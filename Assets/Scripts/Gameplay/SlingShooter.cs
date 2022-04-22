6d3c8a5ff5393c77aae7656a6cb0835b0301d42f
alfianAH <alfian.hamdani04@gmail.com>
2022-04-22 17:29:19 +0800

    Add trajectory

--- Assets/Scripts/Gameplay/SlingShooter.cs
+++ Assets/Scripts/Gameplay/SlingShooter.cs
@@ -5,6 +5,7 @@ namespace Gameplay{
     public class SlingShooter : MonoBehaviour
     {
         public CircleCollider2D shootAreaCollider;
+        public LineRenderer trajectory;
         private Vector2 startPos;
 
         [SerializeField] private float radius = 0.75f;
@@ -29,6 +30,7 @@ namespace Gameplay{
             
             // Set sling shooter to start position
             gameObject.transform.position = startPos;
+            
         }
 
         private void OnMouseDrag()
@@ -40,6 +42,8 @@ namespace Gameplay{
             if (dir.sqrMagnitude > radius)
                 dir = dir.normalized * radius;
             transform.position = startPos + dir;
+
+            
         }
 
         public void InitiateBird(Bird bird)
@@ -47,6 +51,33 @@ namespace Gameplay{
             this.bird = bird;
             this.bird.MoveTo(gameObject.transform.position, gameObject);
             shootAreaCollider.enabled = true;
+        }
+
+        private void DisplayTrajectory(float distance)
+        {
+            if(bird == null) return;
+
+            Vector2 velocity = startPos - (Vector2) transform.position;
+            int segmentCount = 5;
+            Vector2[] segments = new Vector2[segmentCount];
+            
+            // Trajectory's start position is mouse's current position
+            segments[0] = transform.position;
+            
+            // Start velocity
+            Vector2 segVelocity = distance * throwSpeed * velocity;
+            for (int i = 1; i < segmentCount; i++)
+            {
+                float elapsedTime = i * Time.fixedDeltaTime * 5;
+                segments[i] = segments[0] + segVelocity * elapsedTime +
+                    0.5f * Mathf.Pow(elapsedTime, 2) * Physics2D.gravity;
+            }
+            
+            trajectory.positionCount = segmentCount;
+            for (int i = 0; i < segmentCount; i++)
+            {
+                trajectory.SetPosition(i, segments[i]);
+            }
         }
     }
 }
